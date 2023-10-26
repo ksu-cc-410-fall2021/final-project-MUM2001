f91afe6743d722bcbaae0be5fb14f604e9378511
Michael Umscheid <mjumsc@ksu.edu>
2023-10-26 17:03:31 +0000

    Work

--- app/src/main/java/rpg/characterCreation/data/me/myCharacter.java
+++ app/src/main/java/rpg/characterCreation/data/me/myCharacter.java
@@ -1,1 +1,161 @@
 package me;
+
+import data.Character;
+import classes.Class;
+import enums.Talents;
+import races.Race;
+import java.lang.IllegalArgumentException;
+import java.util.LinkedList;
+
+/**
+ * Represents a player character.
+ *
+ * @author: Michael Umscheid mjumsc@ksu.edu
+ * @version: 0.1
+ */
+public class myCharacter implements Character {
+
+    private String name;
+    private Class class;
+    private Race race;
+    private Talent talent;
+    private int health;
+    private int stamina;
+    private int baseHitChance;
+
+
+    /**
+     * Constructor with name provided.
+     *
+     * @param name String representing charcater name
+     */
+    public myCharacter(String name) {
+        if ((name.length() == 0)) {
+            this.name = null;
+        } else {
+            this.name = name;
+        }
+        this.class = null;
+        this.race = null;
+        this.talent = null;
+        this.health = 0;
+        this.stamina = 0;
+        this.baseHitChance = 0;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param name String combo name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return String name of character
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Set Class.
+     *
+     * @param class Class of character
+     */
+    public void setClass(Class class) {
+        this.class = class;
+        this.health += class.healthModifier;
+        this.health += class.staminaModifier;
+    }
+
+    /**
+     * Get Class.
+     *
+     * @return Class class of Character
+     */
+    public Class getClass() {
+        return this.class;
+    }
+
+    /**
+     * Set Race.
+     *
+     * @param race Race of Character
+     */
+    public void setRace(Race race) {
+        this.race = race;
+        this.health += race.healthModifier;
+        this.health += race.staminaModifier;
+    }
+
+    /**
+     * Get Race.
+     *
+     * @return Race race of Character
+     */
+    public Drink getRace() {
+        return this.race;
+    }
+
+    /**
+     * Set Talent.
+     *
+     * @param Talent talent of Character
+     */
+    public void setTalent(Talent talent) {
+        this.talent = talent;
+        if (talent.equals(talent.LEFT_HANDER)) {
+            baseHitChance += 1;
+        } else {
+            if (talent.equals(talent.LEFT_HANDER)) {
+            baseHitChance += 1;
+            }
+        }
+    }
+
+    /**
+     * Get Talent.
+     *
+     * @return Talent talent of Character
+     */
+    public Talent getTalent() {
+        return this.talent;
+    }
+
+    /**
+     * Clear all myCharacter attributes.
+     *
+     *<p>Sets all MyCharacter attributes back to null.
+     */
+    public void clearCharacter() {
+        this.name = null;
+        this.class = null;
+        this.race = null;
+        this.talent = null;
+    }
+
+
+    /**
+     * Get All Items.
+     *
+     * @return LinkedList Items in Character
+     */
+    public LinkedList<Item> getAllItems() {
+        LinkedList<Item> list = new LinkedList<>();
+        if (this.class != null) {
+            list.add(this.class);
+        }
+        if (this.race != null) {
+            list.add(this.race);
+        }
+        if (this.talent != null) {
+            list.add(this.talent);
+        }
+        return list;
+    }
+
+}
--- app/src/main/java/rpg/characterCreation/data/me/myCharacterBuilder.java
+++ app/src/main/java/rpg/characterCreation/data/me/myCharacterBuilder.java
@@ -1,1 +1,116 @@
 package me;
+
+import data.Character;
+import classes.Class;
+import classes.Mage;
+import classes.Rogue;
+import classes.Warrior;
+import perks.Talents;
+import races.Dwarf;
+import races.Elf;
+import races.Human;
+import races.Race;
+import java.lang.IllegalArgumentException;
+
+
+/**
+ * Represents a Builder/Factory Method approach to building myCharacter.
+ *
+ * @author: Michael Umscheid mjumsc@ksu.edu
+ * @version: 0.1
+ */
+public class myCharacterBuilder {
+
+    /**
+     * Builds myCharacter with given name.
+     *
+     * @param name name of desired combo
+     * @return Combo desired combo
+     */
+    public static Combo buildCombo(String name) {
+        if ("OriginalSeries".equals(name)) {
+            return ComboBuilder.buildOriginalSeries();
+        } else if ("NextGeneration".equals(name)) {
+            return ComboBuilder.buildNextGeneration();
+        } else if ("VoyageBeyond".equals(name)) {
+            return ComboBuilder.buildVoyageBeyond();
+        } else if ("GalaxyClass".equals(name)) {
+            return ComboBuilder.buildGalaxyClass();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * Get number of combos available.
+     *
+     * @return int available combos
+     */
+    public static int getNumberOfCombosAvailable() {
+        return 4;
+    }
+
+    /**
+     * Build the OriginalSeries combo.
+     *
+     * @return Combo
+     */
+    private static Combo buildOriginalSeries() {
+        Combo combo = new Combo("OriginalSeries");
+        Entree kirk = new Kirk();
+        Drink worf = new Worf();
+        Side enterprise = new Enterprise(); 
+        combo.setEntree(kirk);
+        combo.setDrink(worf);
+        combo.setSide(enterprise);
+        return combo;
+    }
+
+    /**
+     * Build the NextGeneration combo.
+     *
+     * @return Combo
+     */
+    private static Combo buildNextGeneration() {
+        Combo combo = new Combo("NextGeneration");
+        Entree riker = new Riker();
+        Drink picard = new Picard();
+        Side data = new Data(); 
+        combo.setEntree(riker);
+        combo.setDrink(picard);
+        combo.setSide(data);
+        return combo;
+    }
+
+    /**
+     * Build the VoyageBeyond combo.
+     *
+     * @return Combo
+     */
+    private static Combo buildVoyageBeyond() {
+        Combo combo = new Combo("VoyageBeyond");
+        Entree janeway = new Janeway();
+        Drink troi = new Troi();
+        Side borg = new Borg(); 
+        combo.setEntree(janeway);
+        combo.setDrink(troi);
+        combo.setSide(borg);
+        return combo;
+    }
+
+    /**
+     * Build the GalaxyClass combo.
+     *
+     * @return Combo
+     */
+    private static Combo buildGalaxyClass() {
+        Combo combo = new Combo("GalaxyClass");
+        Entree spock = new Spock();
+        Drink picard = new Picard();
+        Side enterprise = new Enterprise(); 
+        combo.setEntree(spock);
+        combo.setDrink(picard);
+        combo.setSide(enterprise);
+        return combo;
+    }
+}
