f5353394e8fec231cc95bfd5e1e6f9d33ac4936f
Michael Umscheid <mjumsc@ksu.edu>
2023-12-04 16:55:05 +0000

    external library added

---
+++ app/src/main/java/rpg/battle/Dice.java
@@ -0,0 +1,17 @@
+package battle;
+
+import com.wildpi.games.structures.dice.Die;
+import com.wildpi.games.structures.dice.DieBuilder;
+
+/** 
+ * The Dice class(represents dice used in battle mode).
+ *
+ * <p>This class is implements an external dice library to create
+ *    dice objects for use in the battle mode.
+ *
+ * @author Michael Umscheid mjumsc@ksu.edu
+ * @version 0.1
+ */
+public class Dice {
+
+}
